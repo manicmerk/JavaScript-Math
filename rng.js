554c805ca925084b99a49cdf1b4ca55b4f4fb71c
pbemerk <pbemerk@gmail.com>
2020-02-04 00:17:56 -0500

    added a period

--- rng.js
+++ rng.js
@@ -3,4 +3,4 @@ var startNum = parseInt(startValue);
 var topValue = prompt("Pick a larger number to complete the range.");
 var endNum = parseInt(topValue);
 var dieRoll = Math.floor(Math.random() * (endNum - startNum + 1)) + startNum;
-document.write('<p class="size">You rolled a <span class="alive">' + dieRoll + '</span> which is a number between ' + startNum + ' and ' + endNum + '</p>');
+document.write('<p class="size">You rolled a <span class="alive">' + dieRoll + '</span> which is a number between ' + startNum + ' and ' + endNum + '.</p>');

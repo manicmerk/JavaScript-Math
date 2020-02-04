02b2eaf1cdaec0a442b93fd1847439bc66f6ad09
pbemerk <pbemerk@gmail.com>
2020-02-03 20:56:09 -0500

    seconds alive, set

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ var minsPerHour = 60;
 var hoursPerDay = 24;
 var daysPerWeek = 7;
 var weeksPerYear = 52;
+var daysPerYear = 365;
 
 var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
 document.write('<p class="size">There are ' + secondsPerDay + ' seconds in a day</p>');
 
+var yearsAlive = 34;
+var secondsAlive = yearsAlive * weeksPerYear * daysPerWeek * hoursPerDay * minsPerHour * secondsPerMin;
+document.write('<p class="size">I\'ve been alive more than ' + secondsAlive + ' seconds!</p>');

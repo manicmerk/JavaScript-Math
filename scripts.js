f4e42ba5918d646c984bd2cf20d7506004683950
pbemerk <pbemerk@gmail.com>
2020-02-03 20:48:30 -0500

    1 time computation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('<p class="size">There are ' + secondsPerDay + ' seconds in a day</p>');
 

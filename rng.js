8b580b1cc96f19d7e7b3e1e59ba17092a3af22b4
pbemerk <pbemerk@gmail.com>
2020-02-04 00:08:33 -0500

    added seperate num generator with user defined start and end range

---
+++ rng.js
@@ -0,0 +1,6 @@
+var startValue = prompt("Pick a starting a whole number please.");
+var startNum = parseInt(startValue);
+var topValue = prompt("Pick a larger number to complete the range.");
+var endNum = parseInt(topValue);
+var dieRoll = Math.floor(Math.random() * (endNum - startNum + 1)) + startNum;
+document.write('<p class="size">You rolled a <span class="alive">' + dieRoll + '</span> which is a number between ' + startNum + ' and ' + endNum + '</p>');

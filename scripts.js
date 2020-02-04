8146961738e59a849eca7c9d69f4618d79df29d3
pbemerk <pbemerk@gmail.com>
2020-02-03 23:11:28 -0500

    convert string numbers to non string numbers

--- scripts.js
+++ scripts.js
@@ -9,5 +9,8 @@ var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
 document.write('<p class="size">There are ' + secondsPerDay + ' seconds in a day</p>');
 
 var yearsAlive = prompt("What is your current age?");
-var secondsAlive = yearsAlive * daysPerYear * hoursPerDay * minsPerHour * secondsPerMin;
+// Use parseInt() to turn prompt string whole numbers into non-string whole numbers.
+// Use parseFloat() to turn string decimal value numbers into non-string decimal value numbers.
+var secondsAlive = parseInt(yearsAlive) * daysPerYear * hoursPerDay * minsPerHour * secondsPerMin;
 document.write('<p class="size">I\'ve been alive more than <span class="alive">' + secondsAlive + '</span> seconds!</p>');
+
